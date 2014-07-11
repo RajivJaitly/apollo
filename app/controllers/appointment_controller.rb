cad1a510c6f6429cb5b692c90408813c2604389d
krishsana <krishna.lokai@yahoo.com>
2014-07-11 11:12:29 +0530

    forms correction, appointment correction

--- app/controllers/appointment_controller.rb
+++ app/controllers/appointment_controller.rb
@@ -4,7 +4,10 @@ class AppointmentController < ApplicationController
 	end
 
 	def app_form_save
-		debugger
+		Appointment.create(a_pid:params[:fa_pid],
+		              a_did:params[:fa_did],
+		              a_date:params[:fa_date],
+		              a_time:params[:fa_time])
 		redirect_to root_path
 	end
 end
